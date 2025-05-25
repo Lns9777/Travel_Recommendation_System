979effc679a8ede3643b68ed641e60e0af8e5550
Laxmi Narayan Sahu <163241287+Lns9777@users.noreply.github.com>
2025-05-26 01:02:22 +0530

    Delete build_files.sh

--- build_files.sh
+++
@@ -1,7 +0,0 @@
-# pip install -r requirements.txt
-# python3.9 manage.py collectstatic
-#!/bin/bash
-
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput
